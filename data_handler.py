ed43fef1c1dfa358ea09bf097e6a38001c748e3a
Kati Eichenbaum <katieichenbaum@gmail.com>
2019-09-10 16:02:42 +0200

    implement get cards for board

--- data_handler.py
+++ data_handler.py
@@ -17,6 +17,13 @@ def get_boards(cursor):
     return boards
 
 
+@connection_handler
+def get_cards_for_board(cursor, board_id):
+    cursor.execute(f'SELECT title, status_id, placement FROM card WHERE board_id = {board_id}')  # don't know if placement is necessary, let's see
+    cards_for_board = cursor.fetchall()
+    return cards_for_board
+
+
 #def get_card_status(status_id):
  #   """
   #  Find the first status matching the given id
@@ -35,12 +42,16 @@ def get_boards(cursor):
     #return persistence.get_boards(force=True)
 
 
-def get_cards_for_board(board_id):
-    persistence.clear_cache()
-    all_cards = persistence.get_cards()
-    matching_cards = []
-    for card in all_cards:
-        if card['board_id'] == str(board_id):
-            card['status_id'] = get_card_status(card['status_id'])  # Set textual status for the card
-            matching_cards.append(card)
-    return matching_cards
+
+# matching_cards here means the cards that have the same board_id :)
+
+
+# def get_cards_for_board(board_id):
+#     persistence.clear_cache()
+#     all_cards = persistence.get_cards()
+#     matching_cards = []
+#     for card in all_cards:
+#         if card['board_id'] == str(board_id):
+#             card['status_id'] = get_card_status(card['status_id'])  # Set textual status for the card
+#             matching_cards.append(card)
+#     return matching_cards
